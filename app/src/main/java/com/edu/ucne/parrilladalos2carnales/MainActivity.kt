1d10675e3ce367130eb29d861de6cc3303604488
Ramon Junior Urena Vasquez <ramon_urena2@ucne.edu.do>
2026-07-31 20:35:26 -0400

    Initial commit

---
+++ app/src/main/java/com/edu/ucne/parrilladalos2carnales/MainActivity.kt
@@ -0,0 +1,47 @@
+package com.edu.ucne.parrilladalos2carnales
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.edu.ucne.parrilladalos2carnales.ui.theme.ParrilladaLos2CarnalesTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ParrilladaLos2CarnalesTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ParrilladaLos2CarnalesTheme {
+        Greeting("Android")
+    }
+}
